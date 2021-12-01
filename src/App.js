978f48729267a782e712fb09cea0d06060201ad6
solomon stephen <33923021+realrufans@users.noreply.github.com>
2021-12-01 20:11:13 +0100

    initial deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./data";
 
 function App() {
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="headerOptions">
+      <header className="row">
+        <a className="brand" href="index.html">
+          Amazon
+        </a>
+        <a className="cart" href="cart.html">
+          Cart
+        </a>
+        <a className="sign_in" href="signin.html">
+          Sign In
         </a>
       </header>
+
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+
+            <div className="card" key={product._id}>
+
+              <a href={`product/${product._id}`}>
+                
+                <img className="medium" src={product.Image} alt={product.name} />
+              </a>
+              <div className="card-body">
+
+              <a href={`product/${product._id}`}>
+                
+                <h2>{product.name}</h2> 
+              </a>
+
+         
+
+                
+             
+
+                <div className="ratings">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer className="row center">All right reserved!</footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      Image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: "4.5",
+      numReview: 10,
+      decription: "high quality product",
+    },
+    {
+        _id: "2",
+        name: "Adidas Fit Pant",
+        Image: "/images/p2.jpg",
+        price: 139,
+        brand: "Adidas",
+        rating: "3.5",
+        numReview: 10,
+        decription: "high quality product",
+      },
+      {
+        _id: "3",
+        name: "Nike Slim Pant",
+        Image: "/images/p3.jpg",
+        price: 78,
+        brand: "Nike",
+        rating: "5",
+        numReview: 10,
+        decription: "high quality product",
+      },
+      
+  ],
+};
